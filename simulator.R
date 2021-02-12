df443011f752ece6a0df32c50842305aa62128ae
Stas Kolenikov <Stas_Kolenikov@abtassoc.com>
2021-02-12 10:18:21 -0600

    Functions in simulator.R are roxygen-documented. A whole study can now be simulated, summarized, and vaccine efficiency reported.

--- simulator.R
+++ simulator.R
@@ -1,4 +1,34 @@
-# create transition matrix
+#' Create transition matrix for the Markov chain of person trajectories.
+#' 
+#' The transition matrix has have probabilities of transition from a current state
+#' (the row) to the new state (the column), and is intended for the right multiplication
+#' in the linear algebra of Markov chains. 
+#' The probabilities sum up to 1 across the row.
+#' It is best to address the entries of the matrix by their labels.
+#' 
+#' @param dropout_rate Probability of a dropout in a given week (default 0.01).
+#' @param covid_rate   Rate of new cases, per day per 100K
+#' @param v1e          Effectiveness of vaccine 1 (default 0.50).
+#' @param v2e          Effectiveness of vaccine 2 (default 0.50).
+#' @param v3e          Effectiveness of vaccine 3 (default 0.50). 
+#'                     The two vaccine situation can be modeled by setting this to zero.
+#' @param v1r          Probability to get vaccine 1 next week (default 0.01).
+#' @param v2r          Probability to get vaccine 1 next week (default 0.01).
+#' @param v3r          Probability to get vaccine 1 next week (default 0.01).
+#'                     The two vaccine situation can be modeled by setting this to zero.
+#'                    
+#' @return             33x33 Markov chain transition matrix with informatively labeled rows/columns.
+#' 
+#' @examples 
+#' 
+#' Ptransition()
+#' Ptransition(dropout_rate = 0, v1e = 0.9, v2e = 0.5, v3e = 0.3, 
+#'             v1r = (vaccinated_end-vaccinated_start)/52*v1m,
+#'             v2r = (vaccinated_end-vaccinated_start)/52*v1m,
+#'             v3r = (vaccinated_end-vaccinated_start)/52*v1m,
+#'             covid_rate = 50)
+#' 
+#' @export
 Ptransition <- function(
   dropout_rate = 0.01,
   v1e = 0.50, v2e = 0.50, v3e = 0.50, # vaccine efficiency
@@ -160,15 +190,24 @@ Ptransition <- function(
   # return
   return(P)
 }
-P_transition <- Ptransition()
 
-# update 1 person to next week
-
-
-# create one person's trajectory
-person_status <- rep(0,52)
-
-# initial state
+#' Compute the vector of initial probabilities for the Markov chain trajectory
+#' 
+#' @param vaccinated_start   Proportion vaccinated at the start of the study.
+#' @param vm1                Market share of vaccine 1 (default 0.4).
+#' @param vm2                Market share of vaccine 1 (default 0.4).
+#' @param vm3                Market share of vaccine 1 (default 0.2).
+#'                     The two vaccine situation can be modeled by setting this to zero.
+#' @param P_transition       Transition matrix; only needed to inherit the names.      
+#' 
+#' @examples
+#' 
+#' person_status <- rep(0,52)
+#' p_init <- initial_prob(P_transition = P_transition, vaccinated_start = 0.3,
+#'                         vm1 = 0.5, vm2 = 0.3, vm3 = 0.2)
+#' person_status[1] <- sample( 1:length(p_init), size=1, prob=p_init)
+#' 
+#' @export
 initial_prob <- function(
   vaccinated_start = 0.3,
   vm1 = 0.4, vm2 = 0.4, vm3 = 0.2,
@@ -187,14 +226,24 @@ initial_prob <- function(
   return(p)
 }
 
-p_init <- initial_prob(P_transition = P_transition)
-
-person_status[1] <- sample( 1:length(p_init), size=1, prob=p_init)
-for( w in 2:52) {
-  person_status[w] <- sample( 1:length(p_init), size=1, prob=P_transition[person_status[w-1],])
-}
-
-# summarize one person's outcome: weeks no vaccination, weeks Vacc A, B, C; infected
+#' Summarize one person's outcome
+#' 
+#' This function takes a person's 52-week trajectory and provides a summary of person-weeks and statuses.
+#' 
+#' @param person_status   A 52-vector of statuses in the study
+#' @param col_row_names   A 33-vector of labels
+#' 
+#' @return A vector with entries:
+#' 
+#'         weeks_unvaccinated, weeks_vaccinated1, weeks_vaccinated2, weeks_vaccinated3: 
+#'               number from 0 to 52 in this status
+#'         infected_unvaccinated, infected_vaccinated1, infected_vaccinated2,
+#'         infected_vaccinated3, infected_partial, dropped_out:
+#'               0/1 logical variable
+#'               
+#' @examples person_results(person_status, col_row_names = colnames(P_transition))
+#'               
+#' @export
 person_results <- function(person_status, col_row_names) {
   weeks_unvaccinated <- sum(person_status==which(col_row_names ==  "unvaccinated, not infected"))
   weeks_vaccinated1  <- sum(person_status==which(col_row_names ==  "vaccinated 1, not infected"))
@@ -215,27 +264,99 @@ person_results <- function(person_status, col_row_names) {
   dropped_out <- person_status[52] == which(col_row_names ==  "dropped out")
   person_results <- c(weeks_unvaccinated, weeks_vaccinated1, weeks_vaccinated2, weeks_vaccinated3,
                       infected_unvaccinated, infected_vaccinated1, infected_vaccinated2,
-                      infected_vaccinated3, dropped_out)
+                      infected_vaccinated3, infected_partial, dropped_out)
   names(person_results) <- c("weeks_unvaccinated", "weeks_vaccinated1", "weeks_vaccinated2", "weeks_vaccinated3",
                              "infected_unvaccinated", "infected_vaccinated1", "infected_vaccinated2",
-                             "infected_vaccinated3", "dropped_out")
+                             "infected_vaccinated3", "infected_partial", "dropped_out")
   return(person_results)
 }
 
-person_results(person_status, col_row_names = colnames(P_transition))
+#' Simulate site settings
+#' 
+#' @param n_sites   Number of sites, required
+#' @param attr_lo   The lower range of attrition rates
+#' @param attr_hi   The upper range of attrition rates
+#' @param covid_lo  The lower range of attrition rates
+#' @param covid_hi  The upper range of attrition rates
+#' @param seed      Random seed for reproducibility
+#' 
+#' @return          Data frame n_sites x 3 with "Site", "Attrition Rate", "Covid Rate" variables
+#' 
+#' @export
+simulate_site_settings <- function(n_sites, attr_lo, attr_hi, covid_lo, covid_hi, seed=NULL) {
+  
+  if (!is.null(seed)) {
+    set.seed(seed)
+  }
+  
+  df1 <- as.data.frame(matrix(0,nrow=n_sites,3)) # Dummy dataframes to contain the results 
+  colnames(df1) <- c("Site","Attrition Rate","Covid Rate")
+  df1$Site <- paste("Site",1:n_sites)
+  df1$`Attrition Rate` <- runif(n_sites)*(attr_hi-attr_lo) + attr_lo
+  df1$`Covid Rate`     <- runif(n_sites)*(covid_hi-covid_lo) + covid_lo
+  
+  return(df1)
+}
 
-# simulate one site
-simulate_site <- function(
-  seed = 1234,
+#' Simulate vaccination and infection trajectories of one site
+#' 
+#' Based on the input parameters, creates a transition matrix internally,
+#' and runs the 52-week trajectories for the specified number of participants.
+#' 
+#' @param vaccinated_start    Proportion vaccinated at the beginning of the study, default 0.3
+#' @param vaccinated_end      Proportion vaccinated at the beginning of the study, default 0.7
+#' @param vm1                 Market share of vaccine 1 (default 0.4).
+#' @param vm2                 Market share of vaccine 1 (default 0.4).
+#' @param vm3                 Market share of vaccine 1 (default 0.2).
+#'                            The two vaccine situation can be modeled by setting this to zero.
+#' @param v1e                 Effectiveness of vaccine 1 (default 0.50).
+#' @param v2e                 Effectiveness of vaccine 2 (default 0.50).
+#' @param v3e                 Effectiveness of vaccine 3 (default 0.50). 
+#'                            The two vaccine situation can be modeled by setting this to zero.
+#' @param dropout_rate        Probability of a dropout in a given week (default 0.01).
+#' @param covid_rate          Rate of new cases, per day per 100K (default 10)
+#' @param n                   Number of participants per site (default 1000)
+#' @param seed                Random seed for reproducibility
+#' @param verbose             Produce diagnostic output
+#' 
+#' @return                    list with entries
+#'                            $trajectories: the whole (n x 52) matrix of simulated trajectories
+#'                            $summary: the aggregate of person_results()
+#'                            $p_init: the vector of the initial state probabilities
+#'                            $P_transition: the transition matrix
+#'                            $start: the starting state (i.e., the first column of the trajectories)
+#'                            $end: the ending state (i.e., the last column of trajectories)
+#'                            
+#' @examples
+#' 
+#'                            simulate_site_trajectories(n=1000, covid_rate = 100, seed=2, verbose = TRUE)                            
+#'                            
+#' @seealso                   [Ptransition()] for the Markov chain transition matrix,
+#'                            [person_results()] for the aggregation of individual trajectories,
+#'                            [p_init()] for the initial state probabilities                             
+#' @export                            
+simulate_site_trajectories <- function(
+  seed = NULL,
   vaccinated_start = 0.3,
   vaccinated_end = 0.7,
   vm1 = 0.4, vm2 = 0.4, vm3 = 0.2,
   dropout_rate = 0.01,
   v1e = 0.50, v2e = 0.50, v3e = 0.50, # vaccine efficiency
   covid_rate = 10, # new cases per day per 100K
-  n = 1000 # number of simulated persons
+  n = 1000, # number of simulated persons
+  verbose = FALSE
 ) {
-  set.seed(seed)
+  
+  if (!is.null(seed)) {
+    set.seed(seed)
+  }
+  
+  if (verbose) {
+    print("Parameters in simulate_site_trajectories:")
+    print( c(vaccinated_start = vaccinated_start, vaccinated_end = vaccinated_end, 
+             vm1 = vm1, vm2 = vm2, vm3 = vm3, v1e = v1e, v2e = v2e, v3e = v3e, 
+             covid_rate = covid_rate, dropout_rate = dropout_rate) )
+  }
   
   # create the site-specific transition matrix
   P_transition <- Ptransition(dropout_rate = dropout_rate,
@@ -267,12 +388,103 @@ simulate_site <- function(
   for(i in 2:n) {
     summary <- summary + person_results( trajectories[i,], col_row_names = colnames(P_transition))
   }
+  summary <- c(summary, 'n'=n)
   
   return(list(trajectories=trajectories, summary=summary, p_init = p_init, P_transition = P_transition,
               start = trajectories[,1], end = trajectories[,52]))
 }
 
-sim <- simulate_site(n=1000, covid_rate = 100, seed=2)
-sim$summary
-sim$start
-sim$end
+#' Simulate vaccination and infection trajectories of one site
+#' 
+#' Based on the input parameters, creates a transition matrix internally,
+#' and runs the 52-week trajectories for the specified number of participants.
+#' 
+#' @param vaccinated_start    Proportion vaccinated at the beginning of the study, default 0.3
+#' @param vaccinated_end      Proportion vaccinated at the beginning of the study, default 0.7
+#' @param vm1                 Market share of vaccine 1 (default 0.4).
+#' @param vm2                 Market share of vaccine 1 (default 0.4).
+#' @param vm3                 Market share of vaccine 1 (default 0.2).
+#'                            The two vaccine situation can be modeled by setting this to zero.
+#' @param v1e                 Effectiveness of vaccine 1 (default 0.50).
+#' @param v2e                 Effectiveness of vaccine 2 (default 0.50).
+#' @param v3e                 Effectiveness of vaccine 3 (default 0.50). 
+#'                            The two vaccine situation can be modeled by setting this to zero.
+#' @param n                   Number of participants per site (default 1000)
+#' @param seed                Random seed for reproducibility
+#' @param sites               n_sites x 3 data frame with site parameters
+#' @param verbose             Produce more diagnostic output
+#' 
+#' @return                    site_summary: (n_sites x 11) data frame of site summaries
+#'                            
+#' @examples
+#'             df1 <- simulate_site_settings(n_sites = 8, attr_lo = 0.2, attr_hi = 0.5, covid_lo = 10, covid_hi = 50)
+#'             sim_study <- simulate_study(sites=df1)
+#'                            
+#' @seealso                   [Ptransition()] for the Markov chain transition matrix,
+#'                            [person_results()] for the aggregation of individual trajectories,
+#'                            [p_init()] for the initial state probabilities                             
+#' @export                            
+
+simulate_study <- function(
+  verbose = FALSE,
+  seed = NULL,
+  vaccinated_start = 0.3,
+  vaccinated_end = 0.7,
+  vm1 = 0.4, vm2 = 0.4, vm3 = 0.2,
+  v1e = 0.50, v2e = 0.50, v3e = 0.50, # vaccine efficiency
+  sites,   # data frame of site parameters
+  n = 1000 # number of simulated persons
+) {
+
+  if (!is.null(seed)) {
+    set.seed(seed)
+  }
+  
+  if (verbose) {
+    print("Parameters in simulate_study():")
+    print( c(vaccinated_start = vaccinated_start, vaccinated_end = vaccinated_end, 
+             vm1 = vm1, vm2 = vm2, vm3 = vm3, v1e = v1e, v2e = v2e, v3e = v3e, n_sites=nrow(sites), n=n) )
+  }
+  
+  n_sites <- nrow(sites)
+  site_summary <- as.data.frame( matrix(0,nrow=0,ncol=11) )
+  
+  for(k in 1:n_sites) {
+    this_site <- simulate_site_trajectories(
+      verbose = verbose,
+      vaccinated_start = vaccinated_start, vaccinated_end = vaccinated_end,
+      vm1 = vm1, vm2 = vm2, vm3 = vm3, v1e = v1e, v2e = v2e, v3e = v3e,
+      n = n, covid_rate = sites[k,'Covid Rate'],
+      dropout_rate = -log(1-sites[k,'Attrition Rate']/100)/52
+    )
+    site_summary[k,] <- this_site$summary
+  }
+  names(site_summary) <- names(this_site$summary)
+    
+  return(site_summary)
+}  
+
+#' Crude vaccine efficiency statistics
+#' 
+#' @param site_summary  site summary returned by simulate_study()
+#' 
+#' @seealso    [simulate_study()] for the parent function
+#' 
+#' @return     vector with entries for vaccine efficiency named 've1', 've2',  've3'
+#' 
+#' @export
+crude_veff <- function(site_summary) {
+
+  pooled_sim_study <- colSums(site_summary)
+  
+  ir0 <- pooled_sim_study['infected_unvaccinated']/pooled_sim_study['weeks_unvaccinated']
+  ir1 <- pooled_sim_study['infected_vaccinated1']/pooled_sim_study['weeks_vaccinated1']
+  ir2 <- pooled_sim_study['infected_vaccinated2']/pooled_sim_study['weeks_vaccinated2']
+  ir3 <- pooled_sim_study['infected_vaccinated3']/pooled_sim_study['weeks_vaccinated3']
+  
+  ve1 <- 1 - ir1/ir0
+  ve2 <- 1 - ir2/ir0
+  ve3 <- 1 - ir3/ir0
+  
+  return(c('ve1'=unname(ve1), 've2'=unname(ve2), 've3'=unname(ve3) ) )
+}
