11b57bc652421d49915a55ba30d8d5e2d60f99f7
Stas Kolenikov <Stas_Kolenikov@abtassoc.com>
2021-02-15 12:18:28 -0600

    Explainer of the simulation is added as the first tab of the app.

--- app.R
+++ app.R
@@ -65,10 +65,76 @@ ui <- fluidPage(
   mainPanel(
 
     tabsetPanel(
+      tabPanel("Simulation description",
+               p("This tool allows the user to simulate the effects of changes
+                 in vaccination rates, infection rates, and vaccine efficiency. 
+                 The simulation assumptions are:"),
+               p("1. Participants are enrolled at ", n_sites, " sites. 
+                 The user can manipulate the number initially enrolled at each site 
+                 with the corresponding control."),
+               p("2. Study dropout / attrition rates differ by site as indicated by the controls.
+                 The parameter is the expected number of the initial participants who drop out by
+                 the end of the study.
+                 (Site-specific values are simulated from a uniform distribution with these bounds.)"),
+               p("3. Covid infection rates, expressed as new cases per 100K per day, differ by site 
+                  as indicated by the controls.
+                 (Site-specific values are simulated from a uniform distribution with these bounds.)"),
+               p("4. The % of people vaccinated grows through the study period linearly."),
+               p("5. There are three vaccines on the market. Their market shares (i.e., % of 
+                 participants who receive any of these vaccines) are fixed throughout the study
+                 period, do not vary by site."),
+               p("6. Vaccine effectiveness values are fixed. (In particular, this means that one cannot model
+                 different responses of the different vaccines to different strains of the virus.)"),
+               HTML("<p>The <i>Run</i> button simulates the site-specific values of attrition rates
+                 and COVID infection rates (output in the <i>Site parameters</i> tab)."),
+               p("One simulation run consists of:"),
+               p("1. Simulating the specified number of study participants at each site."),
+               p("2. Allocating them in the first week of the study to the vaccinated status 
+                 with the probability given by the 
+                 ''% vaccinated at the beginning of the study'' control,
+                 with proportions vaccinated by the specific products given by their market share."),
+               p("3. Each individual is then progressed to the next week:"),
+               p("3a. Unvaccinated individuals can become vaccinated by one of the vaccines 
+                 with the overall probability controled by the linear growth rate in vaccination
+                 rates as described above; can become infected (with probability controled by 
+                 the site-specific infection rate); can drop out of the study;
+                 or remain unvaccinated, not infected."),
+               p("3b. Individuals that are fully vaccinated can be infected 
+                 (with probability controled by the site-specific infection rate,
+                 and attentuated by the posited effectivness of their respective vaccine),
+                 can drop out; or remain vaccinated, not infected. 
+                 All vaccines are assumed to provide permanent immunity, i.e., there
+                 is no waning of antibodies over time."),
+               p("3c. Individuals who are freshly vaccinated have partial protection that 
+                 increases linearly over the period of 4 weeks. After 4 weeks, they are assumed
+                 fully vaccinated. The individuals who are partially vaccinated can become infected,
+                 or can drop out of the study, or can progress to the next week with increased
+                 protection."),
+               p("4. Individuals who drop out of the study are not followed up, and lost 
+                 to data collection permanantly."),
+               p("5. Other than complete drop out, there are no missing data in the study,
+                 i.e., the status of each person (infected/not infected; vaccinatied/not vaccinated)
+                 is known for every week before the possible dropout."),
+               HTML("The results of a single run of the simulation are obtained with clicking 
+                 <i>Run</i> button. Typically, the simulation takes about 3 to 5 seconds."),
+               HTML("Vaccine effectiveness for vaccine <i>j</i> is estimated as: 
+                 VE<sub>j</sub> = 1 - RR<sub>j</sub> where the relative risk RR<sub>j</sub> is"),
+               withMathJax(HTML("$${\\rm RR}_j = \\frac{{\\rm IR}_{\\mbox{vaccine } j}}{{\\rm IR}_{\\rm unvaccinated}}$$")),
+               withMathJax(
+                 "$${\\rm IR}_{\\rm{vaccine }j} = \\frac{\\mbox{total number infected among fully vaccinated}}{\\mbox{person-weeks fully vaccinated}}$$"),
+               HTML("for vaccine <i>j</i>, and"),
+               withMathJax("$${\\rm IR}_{\\rm unvaccinated} = \\frac{\\mbox{total number infected among unvaccinated}}{\\mbox{person-weeks unvaccinated}}$$"),
+               br("for unvaccinated. Individuals who are infected between their first shot and 4 weeks after the first shot
+                  when full immunity is provided by the vaccine are omitted from either incidence rates."),
+               HTML("The simulation of the individual trajectories is repeated 100 times 
+                 when you click <i>Simulate 100</i> button to produce an approximate
+                 distribution of the estimates of vaccine efficiency as described above.
+                    Correspondingly, this simulation takes about 100 times as long, i.e., about five mintues.")
+               ),
       tabPanel("Site parameters", DT::DTOutput('table1')), 
       tabPanel("Vaccine parameters", DT::DTOutput('table2')), 
-      tabPanel("Example of a single simulation result:", DT::DTOutput('table_simulated_sites')), 
-      tabPanel("Example of estimated vaccine effectiveness:", DT::DTOutput('table_est_veff')), 
+      tabPanel("Example of a single simulation result", DT::DTOutput('table_simulated_sites')), 
+      tabPanel("Example of estimated vaccine effectiveness", DT::DTOutput('table_est_veff')), 
       tabPanel("Distribution of the estimates of vaccine effectiveness based on 100 simulations", plotOutput(outputId = 'veff_distributions', width='100%', height='600px'))
     )
   )
@@ -119,7 +185,7 @@ server <- function(input, output, session) {
     print(df1)
     print(df2)
     
-    df3 <- simulate_study( verbose = TRUE,
+    df3 <- simulate_study( verbose = FALSE,
               sites = df1, n = input$n_per_site,
               vaccinated_start = input$vacc_start/100,
               vaccinated_end   = input$vacc_end/100,
@@ -127,7 +193,7 @@ server <- function(input, output, session) {
               vm2 = df2[2,'Market Share']/100, v2e = df2[2,'Effectiveness']/100, 
               vm3 = df2[3,'Market Share']/100, v3e = df2[3, 'Effectiveness']/100)
     
-    df3
+    df3$site_summary
   }) # end of fdata_simul
   
   observeEvent(input$update, {
@@ -203,7 +269,7 @@ server <- function(input, output, session) {
       scale_linetype_manual("Vaccines", values=c('dashed', 'dotted', 'twodash')) +
       scale_colour_manual("Vaccines", values=c('#7566A0', '#789D4A', '#E87722')) +
       scale_x_continuous(breaks = scales::pretty_breaks(n = 6)) +
-      xlab('Vaccine effectiveness') +
+      xlab('Estimated vaccine effectiveness') +
       theme_light() +
       theme(legend.position = "bottom", legend.box = "horizontal") # legend at the bottom
     }
